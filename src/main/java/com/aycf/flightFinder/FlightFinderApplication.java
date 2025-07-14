e0b8a32e752c2dd90d9486e752875b879c026b1e
Yaakov Haimoff <hyaakov@aol.com>
2025-07-14 23:08:15 +0300

    git init

---
+++ src/main/java/com/aycf/flightFinder/FlightFinderApplication.java
@@ -0,0 +1,13 @@
+package com.aycf.flightFinder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aycf/flightFinder/TestFlightFinderApplication.java
@@ -0,0 +1,11 @@
+package com.aycf.flightFinder;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestFlightFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(FlightFinderApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
